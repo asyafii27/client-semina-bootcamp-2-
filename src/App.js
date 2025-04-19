8e5c4479f79637b78547eb843d3ea549eff253dc
ahmadsyaf27 <ahmadspro99@gmail.com>
2025-04-20 04:17:32 +0700

    button add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// import Button from './componets/Button';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const {name, setName} = useState("");
+  const [age, setAge] = useState(0);
+
+  const klik = () => {
+    setNumber(number + 1);
+
+    console.log(number);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Counter App</h1>
+      <p>Nilai Counter saat ini {number}</p>
+      <button onClick={klik}>Tambah</button>
+
+      <hr/>
+      <h1>Aplikasi Input data diri</h1>
+      <label>Nama</label>
+      <input type="text" value={{name}}/>
+      <label>Usia</label>
+      <input type="number" value={{tahunLahir}}/>
+    </>
   );
 }
 
---
+++ src/componets/Button/Table/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+export default function Table({users}) {
+    return 
+    <table>
+        <thead>
+            <tr>
+                <th>Nomor</th>
+                <th>Nama</th>
+                <th>Usia</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                
+            </tr>
+        </tbody>
+    </table>
+}
