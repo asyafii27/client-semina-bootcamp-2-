818f0bb6bdd7ce1b62c7240ecd412404d5285f91
ahmadsyaf27 <ahmadspro99@gmail.com>
2025-04-21 07:26:40 +0700

    fix: button count

--- src/App.js
+++ src/App.js
@@ -1,31 +1,80 @@
-import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-// import Button from './componets/Button';
+import Input from './components/Input';
 
 function App() {
   const [number, setNumber] = useState(0);
-  const {name, setName} = useState("");
-  const [age, setAge] = useState(0);
+  const [form, setForm] = useState({
+    name: '',
+    usia: '',
+    tahunLahir: ''
+  });
+
+  const [error, setError] = useState('');
+
 
   const klik = () => {
     setNumber(number + 1);
-
     console.log(number);
+  };
+
+  const handleSubmit = () => {
+    if (form.name === "") {
+      setError('Field tidak boleh kosong');
+    } else if (form.tahunLahir == '') {
+      setError('Tahun tidak boleh kosong')
+    } else {
+      const usia = 2025 - parseInt(form.tahunLahir);
+      setForm({ ...form, usia });
+    }
+  };
+
+  const handleChange = (e) => {
+    setError('');
+    if (e.target.name === 'name') {
+      console.log('cek nama' + e.target.value.length);
+      if (e.target.value.length < 3) {
+        console.log('cek nama2');
+        setError('Minimal 3 karakter');
+      }
+    }
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
   };
 
   return (
     <>
       <h1>Counter App</h1>
-      <p>Nilai Counter saat ini {number}</p>
+      <p>Nilai Counter saat ini: {number}</p>
       <button onClick={klik}>Tambah</button>
 
-      <hr/>
-      <h1>Aplikasi Input data diri</h1>
+      <hr />
+      <h1>Aplikasi Input Data Diri</h1>
+
       <label>Nama</label>
-      <input type="text" value={{name}}/>
-      <label>Usia</label>
-      <input type="number" value={{tahunLahir}}/>
+      <Input
+        type="text"
+        name="name"
+        value={form.name}
+        onChange={handleChange}
+      />
+      <br />
+
+      <label>Tahun Lahir</label>
+      <Input
+        type="number"
+        name="tahunLahir"
+        value={form.tahunLahir}
+        onChange={handleChange}
+      />
+      <br />
+      <br />
+
+      <p>Umur saya: {form.usia}</p>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <p style={{color: 'red'}}>{error}</p>
     </>
   );
 }
